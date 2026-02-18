ae163d2d0d1ba8338c92f88e7805594966dcc428
JDevRohanY <rohanyadavudp10@gmail.com>
2026-02-18 23:22:32 +0530

    Implementing create booking api in controller

--- src/main/java/com/lowleveldesign/BookMyShow/controllers/BookingController.java
+++ src/main/java/com/lowleveldesign/BookMyShow/controllers/BookingController.java
@@ -2,18 +2,39 @@ package com.lowleveldesign.BookMyShow.controllers;
 
 import com.lowleveldesign.BookMyShow.dtos.CreateBookingRequestDto;
 import com.lowleveldesign.BookMyShow.dtos.CreateBookingResponseDto;
+import com.lowleveldesign.BookMyShow.dtos.ResponseStatus;
+import com.lowleveldesign.BookMyShow.exceptions.ShowSeatNotFoundException;
+import com.lowleveldesign.BookMyShow.exceptions.UserNotFoundException;
+import com.lowleveldesign.BookMyShow.models.Booking;
+import com.lowleveldesign.BookMyShow.services.BookingService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/bookings")
 public class BookingController {
+    private final BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
     @PostMapping
-    public CreateBookingResponseDto createBooking(@RequestBody CreateBookingRequestDto requestDto){
-        return null;
+    public CreateBookingResponseDto createBooking(@RequestBody CreateBookingRequestDto requestDto) throws UserNotFoundException, ShowSeatNotFoundException {
+        CreateBookingResponseDto responseDto = new CreateBookingResponseDto();
+        Booking booking = null;
+
+        booking = bookingService.createBooking(requestDto.getShowSeatIds(), requestDto.getUserId());
+
+        responseDto.setBooking(booking);
+        responseDto.setResponseStatus(ResponseStatus.SUCCESSFUL);
+
+        if(booking == null){
+            responseDto.setResponseStatus(ResponseStatus.FAILED);
+        }
+
+        return  responseDto;
     }
 }
--- src/main/java/com/lowleveldesign/BookMyShow/dtos/CreateBookingResponseDto.java
+++ src/main/java/com/lowleveldesign/BookMyShow/dtos/CreateBookingResponseDto.java
@@ -1,7 +1,11 @@
 package com.lowleveldesign.BookMyShow.dtos;
 
 import com.lowleveldesign.BookMyShow.models.Booking;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class CreateBookingResponseDto {
     private Booking booking;
     private ResponseStatus responseStatus;
